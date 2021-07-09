97957b7f3bbc68d335270fc970a5402f2c94b9f4
davill02 <artuganov960@gmail.com>
2021-07-09 16:42:26 +0300

    Task 1: Setup

---
+++ bin/custom/training/trainingcockpits/gensrc/org/training/cockpits/constants/GeneratedTrainingCockpitsConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jul 9, 2021 4:16:45 PM                      ---
+ * ----------------------------------------------------------------
+ */
+package org.training.cockpits.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedTrainingCockpitsConstants
+{
+	public static final String EXTENSIONNAME = "trainingcockpits";
+	
+	protected GeneratedTrainingCockpitsConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
